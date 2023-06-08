c2bf17c068f64bcf481a2285ebe586cbfe7f53b7
andregds <andregds@msn.com>
2023-06-08 00:52:49 -0300

    Adicionar Gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,29 +1,24 @@
-// Importar os pacotes do Gulp
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
-// Tarefa de compilação do SASS
 gulp.task('sass', function() {
-  return gulp.src('src/scss/**/*.scss') // caminho para os arquivos SASS
+  return gulp.src('src/scss/**/*.scss') 
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist/css')); // diretório de destino para os arquivos CSS compilados
+    .pipe(gulp.dest('dist/css')); 
 });
 
-// Tarefa de compressão de imagens
 gulp.task('imagemin', function() {
-  return gulp.src('src/images/**/*') // caminho para os arquivos de imagem
+  return gulp.src('src/images/**/*') 
     .pipe(imagemin())
-    .pipe(gulp.dest('dist/images')); // diretório de destino para as imagens comprimidas
+    .pipe(gulp.dest('dist/images'));
 });
 
-// Tarefa de compressão de código JavaScript
 gulp.task('uglify', function() {
-  return gulp.src('src/js/**/*.js') // caminho para os arquivos JavaScript
+  return gulp.src('src/js/**/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js')); // diretório de destino para os arquivos JavaScript comprimidos
+    .pipe(gulp.dest('dist/js'));
 });
 
-// Tarefa padrão do Gulp (executa todas as tarefas)
 gulp.task('default', gulp.parallel('sass', 'imagemin', 'uglify'));
